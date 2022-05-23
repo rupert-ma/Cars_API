3899f5596f3e341218c731a9f91650d23a92408e
rupert-ma <michael.a.rupert@gmail.com>
2022-05-23 11:38:46 -0700

    changed DB setting and created cars app

--- cars_project/settings.py
+++ cars_project/settings.py
@@ -70,15 +70,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'cars_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -121,3 +113,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+try:
+    from cars_project.local_settings import *
+except ImportError:
+    pass
